b1ec3cb6fce7b5fb5fa39e70060edb15618f1910
jayjnu <doublejnu@gmail.com>
2018-01-17 00:00:57 +0900

    Solve merge-sort problem

--- algorithm/sorting/merge-sort.ts
+++ algorithm/sorting/merge-sort.ts
@@ -1,22 +1,58 @@
-/**
- * Problems
- * - Divide and conquer
- * 
- * - end condition: arr.length === 1
- * 
- */
-
-
-interface comparator<T> {
+export interface comparator<T> {
   (...args: T[]): number
 }
 
+/**
+ * Sort an array using merge sort Algorithm
+ * @param arr 
+ * @param compare 
+ */
 export function mergeSort<T>(arr: T[], compare: comparator<T>) {
 
+  /**
+   * Sorting happens in this function
+   * 
+   * Process 1. comapare and merge elements
+   * - compare values of left and right
+   * - remove and push left /right element to merged array,
+   *    until either one is out of element
+   * 
+   * Process 2. 
+   * 
+   * 
+   * @param left 
+   * @param right 
+   */
   function merge(left: T[], right: T[]): T[] {
-    return left
+    const merged:T[] = [];
+
+    // Process 1
+    while(left.length && right.length) {
+      if (compare(left[0], right[0]) <= 0) {
+        merged.push(left.shift())
+      } else {
+        merged.push(right.shift())
+      }
+    }
+
+    while(left.length) {
+      merged.push(left.shift())
+    }
+
+    while(right.length) {
+      merged.push(right.shift())
+    }
+
+    return merged
   }
 
+  /**
+   * Break array down recursively,
+   * until there's only one element and returns array with only one element
+   * 
+   * @param {T} arr 
+   * @returns {T} arr
+   */
   function mergeSortRecursive(arr: T[]): T[] {
     const length = arr.length;
     if (length === 1) {
